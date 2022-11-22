93ee055fe59ca1c2c62d31438259cf5bdc0925b5
Sarah <sarasaaremi@gmail.com>
2022-11-23 00:08:00 +0330

    fix manager suggestions page

--- lib/pages/manager/suggestions.dart
+++ lib/pages/manager/suggestions.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:ieproject/widgets/manager_list_tile.dart';
 import 'package:ieproject/widgets/student_list_tile.dart';
 
-class StudentSuggestions extends StatelessWidget {
+class ManagerSuggestions extends StatelessWidget {
   final titles = ["موضوع 1", "موضوع 2", "موضوع 3"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('پیشنهادهای من'),),
+      appBar: AppBar(title: Text('پیشنهادهای دانشجویان'),),
       floatingActionButton: FloatingActionButton(onPressed: () {  },),
       body: ListView.builder(
           itemCount: titles.length,
           itemBuilder: (context, index) {
-            return Card(child: IEStudentListTile(title: titles[index]));
+            return Card(child: IEManagerListTile(title: titles[index]));
           }),
     );
   }
